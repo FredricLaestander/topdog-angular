8938dfb118fa5ca545ec38dff1bd251347b0ef1c
FredricLaestander <fredriclaestander@gmail.com>
2025-04-28 20:52:45 +0200

    create header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from './components/button/button.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, ButtonComponent, HeaderComponent],
+  template: `<app-header />`,
 })
 export class AppComponent {
   title = 'topdog-angular';
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
+import { LinkComponent } from '../link/link.component';
+
+@Component({
+  selector: 'app-header',
+  imports: [ButtonComponent, LinkComponent],
+  template: `<header class="flex p-4 justify-between">
+    <a href="/">
+      <img src="/logo.svg" alt="logo" />
+    </a>
+
+    <nav class="px-4 flex md:gap-8 items-center">
+      <div class="hidden text-zinc-700 md:flex md:gap-8">
+        <app-link href="/" label="Home" />
+        <app-link href="/createTierList" label="Create tier list" />
+      </div>
+
+      <app-button label="Log in" />
+      <!-- <img src="/profile.svg" alt="profile icon" class="size-8" /> -->
+    </nav>
+  </header>`,
+})
+export class HeaderComponent {}
