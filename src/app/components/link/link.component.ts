15c9962c5094843782aee5b33592bb28f0ba74fc
FredricLaestander <fredriclaestander@gmail.com>
2025-05-06 11:08:42 +0200

    refactor link styles to be inside the component

--- src/app/components/link/link.component.ts
+++ src/app/components/link/link.component.ts
@@ -4,7 +4,7 @@ import { Component, input } from '@angular/core';
   selector: 'app-link',
   imports: [],
   template: `
-    <a [href]="href()" [className]="variant() === 'button' ? 'button' : 'link'">
+    <a [href]="href()" [className]="getClasses()">
       {{ label() }}
     </a>
   `,
@@ -12,5 +12,13 @@ import { Component, input } from '@angular/core';
 export class LinkComponent {
   variant = input<'link' | 'button'>('link');
   href = input.required<string>();
-  label = input.required<string>();
+  label = input<string>();
+
+  getClasses() {
+    if (this.variant() === 'button') {
+      return `flex px-4 py-2 bg-zinc-950 text-white rounded-full hover:bg-zinc-800 transition cursor-pointer`;
+    } else {
+      return `hover:underline hover:text-zinc-950 transition;`;
+    }
+  }
 }
