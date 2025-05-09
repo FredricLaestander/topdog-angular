510be27ac6d6a5b641c32012f0d87015b860c2a8
FredricLaestander <fredriclaestander@gmail.com>
2025-05-09 18:58:38 +0200

    render log in page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { LogInComponent } from './pages/log-in/log-in.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'list/:id', component: ListComponent },
   { path: 'sign-up', component: SignUpComponent },
+  { path: 'log-in', component: LogInComponent },
 ];
