2b3a51035a38e39ded5ebf51dcf8e59a13c4e87d
FredricLaestander <fredriclaestander@gmail.com>
2025-05-13 16:57:17 +0200

    save

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,52 +1,66 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { TierlistService } from '../../services/tierlist.service';
 import { LucideAngularModule, Settings } from 'lucide-angular';
+import { TierComponent } from '../../components/tier/tier.component';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { List } from '../../../types';
+import { AsyncPipe } from '@angular/common';
 @Component({
   selector: 'app-list',
-  imports: [LucideAngularModule],
-  template: `<main class="w-full max-w-5xl p-4 pt-36 gap-8 flex flex-col">
-    <section class="grow flex flex-col gap-3">
-      <div class="flex w-full gap-4">
-        <h2 id="listTitle" class="w-full text-zinc-950 text-xl">
-          My tier list with a very long name
-        </h2>
+  imports: [AsyncPipe, LucideAngularModule, TierComponent],
+  template: `
+    @if (list$ | async; as list) {
+    <main class="w-full max-w-5xl p-4 pt-36 gap-8 flex flex-col">
+      <section class="grow flex flex-col gap-3">
+        <div class="flex w-full gap-4">
+          <h2 id="listTitle" class="w-full text-zinc-950 text-xl">
+            {{ list.name }}
+          </h2>
 
-        <button id="open-settings" class="size-8 p-1 flex cursor-pointer">
-          <lucide-icon
-            [img]="settings"
-            class="size-6 text-zinc-400"
-          ></lucide-icon>
-        </button>
-      </div>
-
-      <p id="description" class="text-zinc-400 text-sm">testar 1-2 1-2</p>
-
-      <div class="flex justify-between w-full">
-        <div class="flex items-center gap-2">
-          <img src="/profile.svg" alt="profile" class="size-6" />
-
-          <p id="username" class="text-xs text-zinc-950"></p>
+          <button id="open-settings" class="size-8 p-1 flex cursor-pointer">
+            <lucide-icon
+              [img]="settings"
+              class="size-6 text-zinc-400"
+            ></lucide-icon>
+          </button>
         </div>
 
-        <p id="error" class="text-red-400"></p>
-      </div>
-    </section>
+        <p id="description" class="text-zinc-400 text-sm">testar 1-2 1-2</p>
 
-    <ul id="tiers" class="w-full">
-      <li class="h-20 md:h-28 flex">
-        <div
-          class="size-20 md:size28 flex shrink-0 items-center justify-center bg-zinc-400"
-        >
-          <p class="text-center break-words min-w-0 line-clamp-4">Tier</p>
+        <div class="flex justify-between w-full">
+          <div class="flex items-center gap-2">
+            <img src="/profile.svg" alt="profile" class="size-6" />
+
+            <p id="username" class="text-xs text-zinc-950"></p>
+          </div>
+
+          <p id="error" class="text-red-400"></p>
         </div>
-        <div
-          class="flex flex-wrap size-full border-y border-r-2 border-zinc-400"
-        ></div>
-      </li>
-    </ul>
-  </main>`,
+      </section>
+
+      <ul id="tiers" class="w-full">
+        <app-tier name="hello" color="#f87171" />
+      </ul>
+    </main>
+    }
+  `,
 })
 export class ListComponent {
   tierlistService = inject(TierlistService);
   readonly settings = Settings;
+
+  route = inject(ActivatedRoute);
+
+  id = computed(() => this.route.snapshot.paramMap.get('id')!);
+  list$!: Observable<List>;
+
+  ngOnInit() {
+    this.list$ = this.tierlistService.getListById(this.id());
+    this.list$.subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+    });
+  }
 }
